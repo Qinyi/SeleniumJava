a6fba1fd9355507dae82c78730d57ae15bb63524
Qinyi Li <liqinyi@hotmail.com>
2018-07-01 19:21:45 -0400

    Initialization
    Initialize the project.

---
+++ src/main/java/Tempo.java
@@ -0,0 +1,10 @@
+public class Tempo {
+    public void printValue(int i) {
+        if (i != 1) {
+            System.out.println("Value equals to 1");
+        } else {
+            System.out.println("Value equals to 1");
+        }
+    }
+
+}
